ccffb39a068ee47a4319b86d7d2a75d57635633a
grandissant <160201222+grandissant@users.noreply.github.com>
2024-02-26 15:34:41 +0900

    Add files via upload

---
+++ Frontend/homepagejs.js
@@ -0,0 +1,4 @@
+document.getElementById('projects').addEventListener('click', function() {
+    // 'Projects' 섹션으로 스크롤
+    document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
+});
